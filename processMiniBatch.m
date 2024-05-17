357b954bf666b67ca78de4d25f0cb45d282428df
AJ Ibraheem <ibraheemaj@outlook.com>
2024-05-17 08:26:28 +0100

    Basic training loop implemented

--- processMiniBatch.m
+++ processMiniBatch.m
@@ -1,14 +1,15 @@
 function [imagesBatch, inputIDs, attentionMask, segmentIDs] = processMiniBatch(images, tokenisedCaption, ~) 
-    segmentIDs = ones(size(tokenisedCaption));
-
     % Hard code the `PaddingValue` for now
     % [~, tokenizer] = bert("Model","tiny");
     % paddingValue = tokenizer.PaddingCode;
     paddingValue = 1; % Hard coded bert tokeniser padding code
-
-    [inputIDs, attentionMask] = padsequences(tokenisedCaption, 2, "PaddingValue", paddingValue);
-    segmentIDs = padsequences(segmentIDs, 2, "PaddingValue", paddingValue);
     
-    % TODO: Image transformation and concatenation
-    keyboard;
+    [inputIDs, attentionMask] = padsequences(tokenisedCaption, 2, "PaddingValue", paddingValue); % Returns in CTB format
+    inputIDs = permute(inputIDs, [1 3 2]);
+    attentionMask = permute(attentionMask, [1 3 2]);
+    segmentIDs = ones(size(inputIDs)); % The `segmentIDs` are always 1, constraint imposed by the `bert` language model
+    
+    % TODO: Move image resizing outside into a transform datastore
+    imagesBatch = cellfun(@(x) imresize(x, [224 224]), images, UniformOutput=false);
+    imagesBatch = cat(4, imagesBatch{:});
 end
